f9e947e1084e0455bcce36ae24b584354a7dfc3b
amant2187 <2021002187.aman@ug.sharda.ac.in>
2022-11-07 20:42:27 +0530

    1.0

--- app/src/main/java/com/amati/hungerbuddy/LoginActivity.java
+++ app/src/main/java/com/amati/hungerbuddy/LoginActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
     Button btn1;
+    EditText edt1 , edt2, edt3 , edt4 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +18,22 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btn1 = findViewById(R.id.btnLogin);
+        edt1 = findViewById(R.id.editTextTextPersonName);
+        edt2 = findViewById(R.id.editTextTextEmailAddress);
+        edt3 = findViewById(R.id.editTextPhone);
+
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
+                if(!(edt1.getText().toString().equals("")) && !(edt2.getText().equals("")) && !(edt3.getText().equals(""))){
+
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                }else{
+                    edt1.setError("blank input");
+                }
+
             }
         });
     }
